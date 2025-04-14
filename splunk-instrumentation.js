81259f4ade1930251eb8360c97a116273c025117
pradeepgattamaneni <gattamanenipradeep@gmail.com>
2025-04-14 12:24:57 -0500

    rum

---
+++ splunk-instrumentation.js
@@ -0,0 +1,7 @@
+import SplunkOtelWeb from '@splunk/otel-web';
+SplunkOtelWeb.init({
+   realm: "us1",
+   rumAccessToken: "4bTPhFMgAGyufn6Pq7ajNQ",
+   applicationName: "pacman",
+   deploymentEnvironment: "prod"
+});
